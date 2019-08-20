4c53a330097261e4bb6e1ca70b27f76e899693b9
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2019-08-20 13:09:27 -0600

    organizing outputs.
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,9 @@
-## this allows the user to do `ssh -F ssh-config default`
-resource "local_file" "ssh_config" {
-  filename = "${path.module}/work/ssh-config"
-  content  = "${data.template_file.ssh_config.rendered}"
-}
-
-output "ssh_config_file" {
-  value = "${local_file.ssh_config.filename}"
-}
-
-output "ssh_private_key" {
-  value = "${module.common.ssh_priv_key_file}"
-}
-
-output "replicated_console_password" {
-  value = "${random_pet.console_password.id}"
-}
-
-output "replicated_console_url" {
-  value = "https://${module.lb.endpoint}:8800"
-}
-
-output "ptfe_endpoint" {
+output "application_endpoint" {
   value = "https://${module.lb.endpoint}"
 }
 
-output "ptfe_health_check" {
+output "application_health_check" {
   value = "https://${module.lb.endpoint}/_health_check"
-}
-
-output "primary_public_ip" {
-  value = "${element(aws_instance.primary.*.public_ip, 0)}"
-}
-
-output "lb_endpoint" {
-  value = "${module.lb.endpoint}"
 }
 
 output "iam_role" {
@@ -42,4 +12,34 @@ output "iam_role" {
 
 output "install_id" {
   value = "${module.common.install_id}"
+}
+
+output "installer_dashboard_password" {
+  value = "${random_pet.console_password.id}"
+}
+
+output "installer_dashboard_url" {
+  value = "https://${module.lb.endpoint}:8800"
+}
+
+output "lb_endpoint" {
+  value = "${module.lb.endpoint}"
+}
+
+## this allows the user to do `ssh -F ssh-config default`
+resource "local_file" "ssh_config" {
+  filename = "${path.module}/work/ssh-config"
+  content  = "${data.template_file.ssh_config.rendered}"
+}
+
+output "primary_public_ip" {
+  value = "${element(aws_instance.primary.*.public_ip, 0)}"
+}
+
+output "ssh_config_file" {
+  value = "${local_file.ssh_config.filename}"
+}
+
+output "ssh_private_key" {
+  value = "${module.common.ssh_priv_key_file}"
 }
