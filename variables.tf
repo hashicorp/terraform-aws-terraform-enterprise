b224c6b83b419ff47cf11997115451b1bd7f2dcd
Amy Brown <amy@hashicorp.com>
2019-09-17 14:10:02 -0500

    typo

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "vpc_id" {
 
 ### =================================================================== OPTIONAL
 
-variable "previx" {
+variable "prefix" {
   type        = "string"
   description = "Name prefix for resource names and tags"
   default     = "tfe"
