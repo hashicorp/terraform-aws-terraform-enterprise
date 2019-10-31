9d83dda6b5b87419c0669c01aaef137c279ae3a4
Roger Steve Ruiz <rruiz@hashicorp.com>
2019-10-30 20:40:11 -0400

    Update examples to use GA release

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "tfe-beta" {
+module "tfe-cluster" {
   source  = "hashicorp/terraform-enterprise/aws"
-  version = "0.0.2-beta"
+  version = "0.0.4"
 
   vpc_id       = "vpc-123456789abcd1234"
   domain       = "example.com"
@@ -13,13 +13,13 @@ module "tfe-beta" {
 
 output "tfe-beta" {
   value = {
-    application_endpoint         = "${module.tfe-beta.application_endpoint}"
-    application_health_check     = "${module.tfe-beta.application_health_check}"
-    iam_role                     = "${module.tfe-beta.iam_role}"
-    install_id                   = "${module.tfe-beta.install_id}"
-    installer_dashboard_password = "${module.tfe-beta.installer_dashboard_password}"
-    installer_dashboard_url      = "${module.tfe-beta.installer_dashboard_url}"
-    primary_public_ip            = "${module.tfe-beta.primary_public_ip}"
-    ssh_private_key              = "${module.tfe-beta.ssh_private_key}"
+    application_endpoint         = "${module.tfe-cluster.application_endpoint}"
+    application_health_check     = "${module.tfe-cluster.application_health_check}"
+    iam_role                     = "${module.tfe-cluster.iam_role}"
+    install_id                   = "${module.tfe-cluster.install_id}"
+    installer_dashboard_password = "${module.tfe-cluster.installer_dashboard_password}"
+    installer_dashboard_url      = "${module.tfe-cluster.installer_dashboard_url}"
+    primary_public_ip            = "${module.tfe-cluster.primary_public_ip}"
+    ssh_private_key              = "${module.tfe-cluster.ssh_private_key}"
   }
 }
