18756c4d9af181f42a6c86064094df8b956ee601
Brandon Ferguson <bnferguson@gmail.com>
2019-11-07 21:12:35 +0100

    Open up port 80 and have the LB forward to 443 for us

--- modules/lb/elb.tf
+++ modules/lb/elb.tf
@@ -37,6 +37,22 @@ resource "aws_lb_target_group" "admin" {
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = "${aws_lb.ptfe.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_lb_listener" "https" {
   load_balancer_arn = "${aws_lb.ptfe.arn}"
   port              = "443"
