08e1980992253a26a727f6cceb7209bcf865b57d
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2019-12-18 10:16:55 -0700

    fix module call for variable update

--- module-common.tf
+++ module-common.tf
@@ -2,7 +2,7 @@ module "common" {
   source             = "./modules/common-user-vpc"
   vpc_id             = var.vpc_id
   subnet_tags        = var.subnet_tags
-  ingress_allow_list = var.allow_list
+  ingress_allow_list = var.ingress_allow_list
   prefix             = var.prefix
 }
 
