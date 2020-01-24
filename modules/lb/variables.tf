179d947a649cd4b6e4be4a384d604a7bf8df1234
Brandon Ferguson <bnferguson@gmail.com>
2020-01-24 13:03:55 +0100

    cast lists correctly

--- modules/lb/variables.tf
+++ modules/lb/variables.tf
@@ -11,17 +11,17 @@ variable "install_id" {
 }
 
 variable "public_subnets" {
-  type        = list
+  type        = list(string)
   description = "list of public subnets"
 }
 
 variable "public_subnets_cidr_blocks" {
-  type        = list
+  type        = list(string)
   description = "list of CIDRs for the public subnets"
 }
 
 variable "private_subnets_cidr_blocks" {
-  type        = list
+  type        = list(string)
   description = "list of CIDRs for the private subnets"
 }
 
