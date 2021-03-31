841352df368f63e0e9485f37a5c379dbfaa72cff
anniehedgpeth <annie@hashicorp.com>
2021-03-31 13:33:56 -0600

    create version.tf in each module

--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.10.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.1.2"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 3.0.0"
-    }
-  }
-}
-
 data "aws_region" "current" {}
 
 data "aws_ami" "ubuntu" {
