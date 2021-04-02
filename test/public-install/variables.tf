d93196389743f19c17c80a29b06114a9a0ff1426
Roger Steve Ruiz <rruiz@hashicorp.com>
2021-04-02 14:11:18 -0400

    Remove some extraneous properties & variables

--- test/public-install/variables.tf
+++ test/public-install/variables.tf
@@ -1,18 +1,3 @@
-variable "prefix" {
-  type        = string
-  default     = "tfe"
-  description = "This prefix is used in the subdomain and friendly_name generation."
-}
-
-variable "tfe_subdomain" {
-  type        = string
-  default     = null
-  description = <<DOC
-    Subdomain for accessing the Terraform Enterprise UI. If this value is null,
-    a random_pet-based name will be generated and used.
-DOC
-}
-
 variable "domain_name" {
   type        = string
   description = "Domain for creating the Terraform Enterprise subdomain on."
@@ -21,12 +6,6 @@ variable "domain_name" {
 variable "license_path" {
   type        = string
   description = "File path to Replicated license file"
-}
-
-variable "existing_aws_keypair" {
-  type        = string
-  default     = null
-  description = "An existing AWS Key Pair to SSH into the Bastion host."
 }
 
 variable "acm_certificate_arn" {
