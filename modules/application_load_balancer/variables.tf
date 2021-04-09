44c811dbb859919a37a96726014b892a5a36bb6d
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-04-09 11:37:30 -0400

    aaron-lane-tflint-delint-application-load-balancer (#94)
    * Add descs. to application_load_balancer outputs
    
    * Add descs., types to alb variables
    
    * Remove unused provider requirements from alb

--- modules/application_load_balancer/variables.tf
+++ modules/application_load_balancer/variables.tf
@@ -1,10 +1,22 @@
-variable "domain_name" {}
+variable "domain_name" {
+  description = "The name of the Route 53 Hosted Zone in which a record will be created."
+  type        = string
+}
 
-variable "admin_dashboard_ingress_ranges" {}
+variable "admin_dashboard_ingress_ranges" {
+  description = "A list of the CIDR blocks from which traffic will be authorized to access the administrator dashboard."
+  type        = list(string)
+}
 
-variable "certificate_arn" {}
+variable "certificate_arn" {
+  description = "The Amazon Resource Name of the SSL certificate to associate with the load balancer."
+  type        = string
+}
 
-variable "fqdn" {}
+variable "fqdn" {
+  description = "The fully qualified domain name to assign to the Route 53 record."
+  type        = string
+}
 
 variable "load_balancing_scheme" {
   default     = "PRIVATE"
@@ -17,12 +29,24 @@ variable "load_balancing_scheme" {
   }
 }
 
-variable "ssl_policy" {}
+variable "ssl_policy" {
+  description = "The name of the SSL policy to assign to the load balancer listeners."
+  type        = string
+}
 
-variable "network_id" {}
+variable "network_id" {
+  description = "The identity of the VPC in which to deploy the load balancer target groups."
+  type        = string
+}
 
-variable "network_public_subnets" {}
-variable "network_private_subnets" {}
+variable "network_public_subnets" {
+  description = "A list of the identities of the public subnetworks in which to deploy the load balancer."
+  type        = list(string)
+}
+variable "network_private_subnets" {
+  description = "A list of the identities of the private subnetworks in which to deploy the load balancer."
+  type        = list(string)
+}
 
 variable "active_active" {
   type        = bool
