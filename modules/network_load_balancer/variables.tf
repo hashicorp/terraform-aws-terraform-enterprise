cf7937c43fb24bfda297d5231c8c70b6c0946912
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-04-09 14:13:40 -0400

    Describe variables; remove unused variables

--- modules/network_load_balancer/variables.tf
+++ modules/network_load_balancer/variables.tf
@@ -1,16 +1,32 @@
-variable "domain_name" {}
+variable "domain_name" {
+  description = "The name of the Route 53 Hosted Zone in which a record will be created."
+  type        = string
+}
 
-variable "admin_dashboard_ingress_ranges" {}
+variable "certificate_arn" {
+  description = "The Amazon Resource Name of the SSL certificate to associate with the load balancer."
+  type        = string
+}
 
-variable "certificate_arn" {}
+variable "fqdn" {
+  description = "The fully qualified domain name to assign to the Route 53 record."
+  type        = string
+}
 
-variable "fqdn" {}
+variable "ssl_policy" {
+  description = "The name of the SSL policy to assign to the load balancer listeners."
+  type        = string
+}
 
-variable "ssl_policy" {}
+variable "network_id" {
+  description = "The identity of the VPC in which to deploy the load balancer target groups."
+  type        = string
+}
 
-variable "network_id" {}
-
-variable "network_private_subnets" {}
+variable "network_private_subnets" {
+  description = "A list of the identities of the private subnetworks in which to deploy the load balancer."
+  type        = list(string)
+}
 
 variable "active_active" {
   type        = bool
