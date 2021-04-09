fb0c46abb8723e7212e20c7274d599dff1428da3
Roger Steve Ruiz <rruiz@hashicorp.com>
2021-04-09 16:55:24 -0400

    Update backend configuration with required options

--- tests/public-install/versions.tf
+++ tests/public-install/versions.tf
@@ -1,5 +1,11 @@
 terraform {
-  backend "remote" {}
+  backend "remote" {
+    organization = "terraform-enterprise-modules-test"
+
+    workspaces {
+      name = "aws-public-active-active"
+    }
+  }
   required_version = ">= 0.13"
   required_providers {
     random = {
