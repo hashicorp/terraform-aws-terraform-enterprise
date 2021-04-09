e2a505c639c0aa17bec7fd9bb6cc55b45dfdcf15
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-04-09 16:05:33 -0400

    aaron-lane-delint-network-load-balancer (#97)
    * Add descriptions to outputs
    
    * Describe variables; remove unused variables
    
    * Remove unused provider requirements

--- modules/network_load_balancer/versions.tf
+++ modules/network_load_balancer/versions.tf
@@ -5,17 +5,5 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.10"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.1"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 3.0"
-    }
   }
 }
