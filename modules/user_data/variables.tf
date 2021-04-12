9dffdfe68d4976afd03b7501eba3012fd7d446a2
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-04-12 11:49:34 -0400

    aaron-lane-delint-user-data (#104)
    * Describe outputs
    
    * Describe, typify variables; remove unused vars
    
    * Drop unused provider requirements

--- modules/user_data/variables.tf
+++ modules/user_data/variables.tf
@@ -1,23 +1,51 @@
 
-variable "fqdn" {}
-variable "active_active" {
-  default = true
-  type    = bool
+variable "fqdn" {
+  description = "The fully qualified domain name of the load balancer."
+  type        = string
 }
-variable "generated_bastion_key_private" {}
-variable "aws_bucket_bootstrap" {}
-variable "aws_bucket_data" {}
-variable "aws_region" {}
-variable "tfe_license" {}
-variable "kms_key_arn" {}
+variable "active_active" {
+  default     = true
+  description = "A boolean that indicates if the TFE deployment is Active/Active or Standalone."
+  type        = bool
+}
+variable "generated_bastion_key_private" {
+  description = "The PEM formatted private data of the SSH key pair associated with the bastion EC2 instance."
+  type        = string
+}
+variable "aws_bucket_bootstrap" {
+  description = "The name of the S3 storage bucket which contains TFE bootstrap artifacts."
+  type        = string
+}
+variable "aws_bucket_data" {
+  description = "The name of the S3 storage bucket which contains TFE runtime data."
+  type        = string
+}
+variable "aws_region" {
+  description = "The region in which the S3 storage bucket which contains TFE runtime data is deployed."
+  type        = string
+}
+variable "tfe_license" {
+  description = "The name of the S3 storage bucket object which contains the TFE license."
+  type        = string
+}
+variable "kms_key_arn" {
+  description = "The Amazon Resource Name of the KMS key which is used to encrypt S3 storage bucket objects."
+  type        = string
+}
 variable "redis_host" {
-  default = ""
+  default     = ""
+  description = "The IP address of the primary node in the Redis Elasticache replication group."
+  type        = string
 }
 variable "redis_pass" {
-  default = ""
+  default     = ""
+  description = "The password which is required to create connections with the Redis Elasticache replication group."
+  type        = string
 }
 variable "redis_port" {
-  default = ""
+  default     = ""
+  description = "The port number on which the Redis Elasticache replication group accepts connections."
+  type        = string
 }
 
 variable "redis_use_password_auth" {
@@ -32,12 +60,27 @@ variable "redis_use_tls" {
   description = "Determines if the Replicated configuration is aware to use TLS/HTTPS."
 }
 
-variable "pg_netloc" {}
-variable "pg_dbname" {}
-variable "pg_user" {}
-variable "pg_password" {}
+variable "pg_netloc" {
+  description = "The connection endpoint of the PostgreSQL RDS instance in address:port format."
+  type        = string
+}
+variable "pg_dbname" {
+  description = "The name of the PostgreSQL RDS instance."
+  type        = string
+}
+variable "pg_user" {
+  description = "The name of the main PostgreSQL user."
+  type        = string
+}
+variable "pg_password" {
+  description = "The password of the main PostgreSQL user."
+  type        = string
+}
 
-variable "proxy_ip" {}
+variable "proxy_ip" {
+  description = "The IP address of the HTTP proxy through which TFE traffic will be routed."
+  type        = string
+}
 variable "proxy_cert_bundle_name" {
   type        = string
   description = "(Optional) name of cert bundle stored in S3"
@@ -48,5 +91,3 @@ variable "no_proxy" {
   description = "(Optional) List of IP addresses to not proxy"
   default     = []
 }
-
-variable "friendly_name_prefix" {}
