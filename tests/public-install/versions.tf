cef17549a52a66b70bb6944fb4650bf15bf9d242
Roger Steve Ruiz <rruiz@hashicorp.com>
2021-04-21 14:00:36 -0400

    Add in the required_providers block for AWS

--- tests/public-install/versions.tf
+++ tests/public-install/versions.tf
@@ -8,6 +8,10 @@ terraform {
   }
   required_version = ">= 0.13"
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.10"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1"
