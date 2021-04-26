a33e989db9375c535140d12c0a164638a17aef47
Roger Steve Ruiz <rruiz@hashicorp.com>
2021-04-26 13:09:44 -0400

    Wait for TFE (#128)
    * Add a health_check_url output to root & tests
    
    * Define an anchor for the public working directory
    
    * Add in the "Wait For TFE" step with a 15m timeout

--- tests/public-install/outputs.tf
+++ tests/public-install/outputs.tf
@@ -2,3 +2,8 @@ output "public_install" {
   value       = module.public_install
   description = "The output of all the public_install module."
 }
+
+output "health_check_url" {
+  value       = module.public_install.health_check_url
+  description = "The health check URL for TFE."
+}
