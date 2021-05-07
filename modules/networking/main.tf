98f5774aa12258b8e7f5c7b8daf3cf3239d614ab
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-05-07 16:40:41 -0400

    Add endpoints: ec2messages; s3; ssm; ssmmessages

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -52,116 +52,43 @@ module "vpc" {
   }
 }
 
-#################################################
-# Internet Gateway
-#################################################
-resource "aws_internet_gateway" "igw" {
-  count = var.deploy_vpc == true ? 1 : 0
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "~> 3.0"
 
-  vpc_id = aws_vpc.main[0].id
+  vpc_id = module.vpc.vpc_id
+  tags   = var.common_tags
 
-  tags = merge(
-    { Name = "${var.friendly_name_prefix}-tfe-igw" },
-    var.common_tags
-  )
-}
-
-#################################################
-# Elastic IPs
-#################################################
-resource "aws_eip" "nat_eip" {
-  count = var.deploy_vpc == true && length(var.network_public_subnet_cidrs) > 0 ? length(var.network_public_subnet_cidrs) : 0
-
-  vpc        = true
-  depends_on = [aws_internet_gateway.igw]
-
-  tags = merge(
-    { Name = "${var.friendly_name_prefix}-tfe-nat-eip" },
-    var.common_tags
-  )
-}
-
-#################################################
-# NAT Gateways
-#################################################
-resource "aws_nat_gateway" "ngw" {
-  count = var.deploy_vpc == true && length(var.network_public_subnet_cidrs) > 0 ? length(var.network_public_subnet_cidrs) : 0
-
-  allocation_id = element(aws_eip.nat_eip.*.id, count.index)
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
-
-  depends_on = [
-    aws_internet_gateway.igw,
-    aws_eip.nat_eip,
-    aws_subnet.public,
-  ]
-
-  tags = merge(
-    { Name = "${var.friendly_name_prefix}-tfe-ngw-${count.index}" },
-    var.common_tags
-  )
-}
-
-#################################################
-# Route Tables & Routes
-#################################################
-resource "aws_route_table" "rtb_public" {
-  count = var.deploy_vpc == true ? 1 : 0
-
-  vpc_id = aws_vpc.main[0].id
-
-  depends_on = [aws_internet_gateway.igw]
-
-  tags = merge(
-    { Name = "${var.friendly_name_prefix}-tfe-rtb-public" },
-    var.common_tags
-  )
-}
-
-resource "aws_route" "route_public" {
-  count = var.deploy_vpc == true ? 1 : 0
-
-  route_table_id         = aws_route_table.rtb_public[0].id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw[0].id
-}
-
-resource "aws_route_table" "rtb_private" {
-  count = var.deploy_vpc == true ? length(var.network_private_subnet_cidrs) : 0
-
-  vpc_id = aws_vpc.main[0].id
-
-  depends_on = [aws_nat_gateway.ngw]
-
-  tags = merge(
-    { Name = "${var.friendly_name_prefix}-tfe-rtb-private-${count.index}" },
-    var.common_tags
-  )
-}
-
-resource "aws_route" "route_private" {
-  count = var.deploy_vpc == true ? length(var.network_private_subnet_cidrs) : 0
-
-  route_table_id         = element(aws_route_table.rtb_private.*.id, count.index)
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
-}
-
-resource "aws_route_table_association" "rtbassoc_public" {
-  count = var.deploy_vpc == true ? length(var.network_public_subnet_cidrs) : 0
-
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.rtb_public[0].id
-}
-
-resource "aws_route_table_association" "rtbassoc_private" {
-  count = var.deploy_vpc == true ? length(var.network_private_subnet_cidrs) : 0
-
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.rtb_private.*.id, count.index)
-}
-
-locals {
-  network_id          = var.deploy_vpc == true ? aws_vpc.main[0].id : ""
-  bastion_host_subnet = var.deploy_vpc == true ? aws_subnet.public[0].id : ""
+  endpoints = {
+    ec2messages = {
+      private_dns_enabled = true
+      service             = "ec2messages"
+      subnet_ids          = module.vpc.private_subnets
+      tags = {
+        Name = "${var.friendly_name_prefix}-tfe-ec2messages-vpc-endpoint"
+      }
+    }
+    s3 = {
+      service = "s3"
+      tags = {
+        Name = "${var.friendly_name_prefix}-tfe-s3-vpc-endpoint"
+      }
+    }
+    ssm = {
+      private_dns_enabled = true
+      service             = "ssm"
+      subnet_ids          = module.vpc.private_subnets
+      tags = {
+        Name = "${var.friendly_name_prefix}-tfe-ssm-vpc-endpoint"
+      }
+    }
+    ssmmessages = {
+      private_dns_enabled = true
+      service             = "ssmmessages"
+      subnet_ids          = module.vpc.private_subnets
+      tags = {
+        Name = "${var.friendly_name_prefix}-tfe-ssmmessages-vpc-endpoint"
+      }
+    }
+  }
 }
