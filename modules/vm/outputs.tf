32c8ec4760c2bc6cfef44c271400d4c78e5fd592
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-05-12 16:19:39 -0400

    Add more sensitive bug workarounds

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -8,4 +8,6 @@ output "tfe_autoscaling_group" {
   value = aws_autoscaling_group.tfe_asg
 
   description = "The autoscaling group which hosts the TFE EC2 instance(s)."
+  # This output is marked as sensitive to work around a bug in Terraform 0.14
+  sensitive = true
 }
