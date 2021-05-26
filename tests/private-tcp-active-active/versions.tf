1e595459db971370882d1c493363c2b56dc9fbf1
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-05-26 17:02:01 -0400

    Remove unused provider requirements

--- tests/private-tcp-active-active/versions.tf
+++ tests/private-tcp-active-active/versions.tf
@@ -13,17 +13,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.15"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.1"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 3.0"
     }
   }
 }
