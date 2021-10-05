47c01cbb59d73d28fe32239666919b2cb9826fce
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-10-05 14:52:02 -0400

    Remove bootstrap from object_storage

--- modules/object_storage/variables.tf
+++ modules/object_storage/variables.tf
@@ -3,36 +3,7 @@ variable "kms_key_arn" {
   type        = string
 }
 
-variable "tfe_license_name" {
-  type        = string
-  default     = "ptfe-license.rli"
-  description = "(Required) Filename for PTFE license file."
-}
-
-variable "tfe_license_filepath" {
-  type        = string
-  description = "(Required) Absolute filepath to location of PTFE license file."
-}
-
-variable "external_bootstrap_bucket" {
-  type        = string
-  description = "The name of the S3 bucket for Replicated licenses."
-  default     = null
-}
-
 variable "friendly_name_prefix" {
   type        = string
   description = "(Required) Friendly name prefix used for tagging and naming AWS resources."
-}
-
-variable "proxy_cert_bundle_filepath" {
-  description = "The pathname of the certificate bundle file for the HTTP proxy."
-  type        = string
-}
-
-variable "proxy_cert_bundle_name" {
-  description = <<-EOD
-  The name to assign to the S3 storage bucket object which will contain the certificate bundle file for the HTTP proxy.
-  EOD
-  type        = string
 }
