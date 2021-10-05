9316d8cd394fe4451503efb9057dbe7d5365d96a
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2021-10-05 14:52:46 -0400

    Update service_accounts for secrets

--- modules/service_accounts/variables.tf
+++ modules/service_accounts/variables.tf
@@ -3,11 +3,6 @@ variable "friendly_name_prefix" {
   description = "(Required) Friendly name prefix used for tagging and naming AWS resources."
 }
 
-variable "aws_bucket_bootstrap_arn" {
-  description = "The Amazon Resource Name of the S3 storage bucket which contains TFE bootstarp artifacts."
-  type        = string
-}
-
 variable "aws_bucket_data_arn" {
   description = "The Amazon Resource Name of the S3 storage bucket whih contains TFE runtime data."
   type        = string
@@ -22,4 +17,21 @@ variable "iam_role_policy_arns" {
   default     = []
   description = "A set of Amazon Resource Names of IAM role policys to be attached to the TFE IAM role."
   type        = set(string)
+}
+
+variable "ca_certificate_secret" {
+  type = object({
+    arn = string
+  })
+  description = <<-EOD
+  A Secrets Manager secret which contains the Base64 encoded version of a PEM encoded public certificate of a
+  certificate authority (CA) to be trusted by the EC2 instance.
+  EOD
+}
+
+variable "tfe_license_secret" {
+  type = object({
+    arn = string
+  })
+  description = "The Secrets Manager secret under which the Base64 encoded Terraform Enterprise license is stored."
 }
