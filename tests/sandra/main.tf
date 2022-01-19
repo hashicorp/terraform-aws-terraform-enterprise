8705bd114602ae77fa4e642b3e8ed5ecc1bed22f
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-01-19 11:21:12 -0500

    lint fix

--- tests/sandra/main.tf
+++
@@ -1,7 +0,0 @@
-module "secrets" {
-  source = "../../fixtures/secrets"
-  tfe_license = {
-    name = "my-tfe-license"
-    path = var.tfe_license.path
-  }
-}
--- tests/sandra/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "tfe_license" {
-  default = null
-  type = object({
-    name = string
-    path = string
-  })
-  description = "A map that consists of the newly created secret name and the local path to the Terraform Enterprise license."
-}
