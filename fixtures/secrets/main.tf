369f28de5055be80ea2f00b1523fbf1f24f4391c
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-01-20 17:27:55 -0500

    adding ca_certificate

--- fixtures/secrets/main.tf
+++ fixtures/secrets/main.tf
@@ -1,9 +1,57 @@
 resource "aws_secretsmanager_secret" "tfe_license" {
+  count = var.tfe_license == null ? 0 : 1
   description = "The TFE license."
 }
 
 resource "aws_secretsmanager_secret_version" "tfe_license" {
-  secret_binary = filebase64("${var.tfe_license.path}")
-  secret_id     = aws_secretsmanager_secret.tfe_license.id
+  count = var.tfe_license == null ? 0 : 1
+  secret_binary = filebase64(var.tfe_license.path)
+  secret_id     = aws_secretsmanager_secret.tfe_license[count.index].id
 }
+
+resource "aws_secretsmanager_secret" "ca_certificate" {
+  count = var.ca_certificate == null ? 0 : 1
+  description = "The CA certificate"
+}
+
+resource "aws_secretsmanager_secret_version" "ca_certificate" {
+  count = var.ca_certificate == null ? 0 : 1
+  secret_binary = filebase64(var.ca_certificate.path)
+  secret_id     = aws_secretsmanager_secret.ca_certificate[count.index].id
+}
+
+
+resource "aws_secretsmanager_secret" "ca_private_key" {
+  count = var.ca_private_key == null ? 0 : 1
+  description = "CA Certificate private key"
+}
+
+resource "aws_secretsmanager_secret_version" "ca_private_key" {
+  count = var.ca_private_key == null ? 0 : 1
+  secret_binary = filebase64(var.ca_private_key.path)
+  secret_id     = aws_secretsmanager_secret.ca_private_key[count.index].id
+}
+
+resource "aws_secretsmanager_secret" "ssl_certificate" {
+  count = var.ssl_certificate == null ? 0 : 1
+  description = "SSl certificate"
+}
+
+resource "aws_secretsmanager_secret_version" "ssl_certificate" {
+  count = var.ssl_certificate == null ? 0 : 1
+  secret_binary = filebase64(var.ssl_certificate.path)
+  secret_id     = aws_secretsmanager_secret.ssl_certificate[count.index].id
+}
+
+resource "aws_secretsmanager_secret" "ssl_private_key" {
+  count = var.ssl_private_key == null ? 0 : 1
+  description = "SSL certificate private key"
+}
+
+resource "aws_secretsmanager_secret_version" "ssl_private_key" {
+  count = var.ssl_private_key == null ? 0 : 1
+  secret_binary = filebase64(var.ssl_private_key.path)
+  secret_id     = aws_secretsmanager_secret.ssl_private_key[count.index].id
+}
+
 
--- fixtures/secrets/output.tf
+++ fixtures/secrets/output.tf
@@ -1,3 +1,20 @@
 output "tfe_license" {
-  value = aws_secretsmanager_secret_version.tfe_license.secret_id
+  value = aws_secretsmanager_secret_version.tfe_license[0].secret_id
 }
+
+output "tfe_license" {
+  value = aws_secretsmanager_secret_version.ca_certificate[0].secret_id
+}
+
+output "tfe_license" {
+  value = aws_secretsmanager_secret_version.ca_private_key[0].secret_id
+}
+
+output "tfe_license" {
+  value = aws_secretsmanager_secret_version.ssl_certificate[0].secret_id
+}
+
+output "tfe_license" {
+  value = aws_secretsmanager_secret_version.ssl_private_key[0].secret_id
+}
+
