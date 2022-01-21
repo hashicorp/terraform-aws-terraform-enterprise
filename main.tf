1c22adf8ef0a4a8a0fa9e921e6189e8568e689b6
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-01-21 10:38:03 -0500

    test

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "secrets" {
 module "service_accounts" {
   source = "./modules/service_accounts"
 
-  ca_certificate_secret = var.ca_certificate_secret == null ? var.ca_certificate_secret : module.secrets.ca_certificate_secret
+  ca_certificate_secret = var.ca_certificate_secret != null ? var.ca_certificate_secret : module.secrets.ca_certificate_secret
   friendly_name_prefix  = var.friendly_name_prefix
   iam_role_policy_arns  = var.iam_role_policy_arns
   tfe_license_secret    = var.tfe_license_secret
@@ -105,7 +105,7 @@ module "user_data" {
   iact_subnet_list       = var.iact_subnet_list
   iact_subnet_time_limit = var.iact_subnet_time_limit
   kms_key_arn            = module.kms.key.arn
-  ca_certificate_secret  = var.ca_certificate_secret == null ? var.ca_certificate_secret : module.secrets.ca_certificate_secret
+  ca_certificate_secret  = var.ca_certificate_secret != null ? var.ca_certificate_secret : module.secrets.ca_certificate_secret
 
   # Postgres
   pg_dbname   = module.database.db_name
