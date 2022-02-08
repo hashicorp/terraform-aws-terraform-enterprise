4e1e5f6de03becfdbfe271458fdddb6e2eaab8e3
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-02-08 08:42:47 -0500

    Update examples/standalone-mounted/main.tf
    Co-authored-by: Annie Hedgpeth <annie@hashicorp.com>

--- examples/standalone-mounted/main.tf
+++ examples/standalone-mounted/main.tf
@@ -33,7 +33,7 @@ resource "aws_key_pair" "main" {
 module "secrets" {
   source = "../../fixtures/secrets"
   tfe_license = {
-    name = "${var.friendly_name_prefix}-tfe-license"
+    name = "tfe-license"
     path = var.license_file
   }
 }
