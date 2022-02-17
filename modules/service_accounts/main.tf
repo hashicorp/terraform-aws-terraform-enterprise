791ca6437836587df6d6db37b19e95c2358ba22b
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-02-17 16:10:18 -0500

    attached kms policy

--- modules/service_accounts/main.tf
+++ modules/service_accounts/main.tf
@@ -63,6 +63,10 @@ resource "aws_iam_role_policy_attachment" "misc" {
   policy_arn = each.value
 }
 
+resource "aws_iam_role_policy_attachment" "kms_policy" {
+  role       = aws_iam_role.instance_role.name
+  policy_arn = var.kms_key_arn
+}
 
 resource "aws_iam_policy" "kms_policy" {
   name = "${var.friendly_name_prefix}-key"
