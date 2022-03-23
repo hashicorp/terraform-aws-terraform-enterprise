e384a6bffe78df49665adc51fc564372ad2b71a9
John Kerry <john.kerry@hashicorp.com>
2022-03-22 21:26:12 -0400

    adding ubuntu ami data source

--- tests/public-active-active/data.tf
+++ tests/public-active-active/data.tf
@@ -1,3 +1,19 @@
 data "aws_secretsmanager_secret" "tfe_license" {
   name = var.tfe_license_secret_name
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
