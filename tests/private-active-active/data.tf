43a75c6d0643ae903a1e162728e16e3a1651ce00
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-03-28 15:08:42 -0400

    lint fix

--- tests/private-active-active/data.tf
+++ tests/private-active-active/data.tf
@@ -17,33 +17,3 @@ data "aws_ami" "rhel" {
     values = ["hvm"]
   }
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-data "aws_iam_policy_document" "proxy_instance_role" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
