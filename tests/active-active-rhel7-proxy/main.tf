a11fa240b97342a4939d98eddfc9f3135debd3d3
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-03-31 12:14:40 -0400

    Update main.tf

--- tests/active-active-rhel7-proxy/main.tf
+++ tests/active-active-rhel7-proxy/main.tf
@@ -73,7 +73,7 @@ module "tfe" {
   load_balancing_scheme    = local.load_balancing_scheme
   object_storage_iam_user  = data.aws_iam_user.object_storage
   node_count               = 2
-  proxy_ip                 = "${module.test_proxy.proxy_ip}:${local.http_proxy_port}"
+  proxy_ip                 = module.test_proxy.proxy_ip
   proxy_port               = local.http_proxy_port
   tfe_subdomain            = local.test_name
 
