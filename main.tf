f0230d8bb2d23f1b14520586b8961ab70f7ce31e
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2022-04-22 17:44:21 -0400

    Update reference

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ module "database" {
 # TFE and Replicated settings to pass to the tfe_init module
 # -----------------------------------------------------------------------------
 module "settings" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/settings?ref=main"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/settings?ref=tf-440"
 
   # TFE Base Configuration
   custom_image_tag       = var.custom_image_tag
