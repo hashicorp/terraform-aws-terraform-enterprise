c35087ed6cc2921f4be1f118c9969bf4158d2db4
John Kerry <john.kerry@hashicorp.com>
2022-07-07 11:14:28 -0400

    Upgrading hashicorp/aws provider to ~>4.0

--- modules/application_load_balancer/versions.tf
+++ modules/application_load_balancer/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.72"
+      version = "~> 4.0"
     }
   }
 }
--- tests/public-active-active/versions.tf
+++ tests/public-active-active/versions.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.72"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
