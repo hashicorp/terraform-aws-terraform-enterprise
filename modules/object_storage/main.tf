f76864baa04ea31c646fd05afb96eb5f4759ab50
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2022-07-14 10:05:16 -0400

    terraform fmt

--- modules/object_storage/main.tf
+++ modules/object_storage/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tfe_data_bucket" {
-  bucket = "${var.friendly_name_prefix}-tfe-data"
+  bucket        = "${var.friendly_name_prefix}-tfe-data"
   force_destroy = true
 }
 
