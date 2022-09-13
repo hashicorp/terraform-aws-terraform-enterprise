6989aed7019e1429e3b2d7139776e0ff3a5c33d6
Sandra Maria Peter <83961684+sandrampeter@users.noreply.github.com>
2022-09-13 11:02:11 -0400

    update

--- tests/standalone-vault/versions.tf
+++ tests/standalone-vault/versions.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 0.14"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1"
