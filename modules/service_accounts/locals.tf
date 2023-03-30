f07dbccbf72a20a296d1154de0e228ac0830bb38
anniehedgpeth <annie@hashicorp.com>
2023-03-30 17:44:04 -0600

    change to a more descriptive data local name

--- modules/service_accounts/locals.tf
+++ modules/service_accounts/locals.tf
@@ -9,6 +9,6 @@ locals {
     var.vm_key_secret_id
   ] : secret if secret != null]
 
-  iam_instance_role    = try(data.aws_iam_role.instance_role[0], aws_iam_role.instance_role[0])
-  iam_instance_profile = try(data.aws_iam_instance_profile.tfe[0], aws_iam_instance_profile.tfe[0])
+  iam_instance_role    = try(data.aws_iam_role.existing_instance_role[0], aws_iam_role.instance_role[0])
+  iam_instance_profile = try(data.aws_iam_instance_profile.existing_instance_profile[0], aws_iam_instance_profile.tfe[0])
 }
