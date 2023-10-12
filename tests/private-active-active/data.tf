d4b70ea99a84350d091c158ac3a23db0b8a1f207
anniehedgpeth <annie@hashicorp.com>
2023-10-12 14:56:45 -0600

    secret data blocks

--- tests/private-active-active/data.tf
+++ tests/private-active-active/data.tf
@@ -1,6 +1,14 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+data "aws_secretsmanager_secret" "vm_key" {
+  name = "wildcard-private-key-pem"
+}
+
+data "aws_secretsmanager_secret" "vm_certificate" {
+  name = "wildcard-chained-certificate-pem"
+}
+
 data "aws_ami" "rhel" {
   owners = ["309956199498"] # RedHat
 
