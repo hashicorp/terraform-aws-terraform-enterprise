ef5a94c85944194bcde7a7c426d8feea393234fe
Author Name <5195704+chrisfishwood@users.noreply.github.com>
2023-11-02 19:43:08 -0700

    poc of TFE with aws aurora

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -53,33 +53,118 @@ resource "aws_db_subnet_group" "tfe" {
   subnet_ids = var.network_subnets_private
 }
 
-resource "aws_db_instance" "postgresql" {
-  allocated_storage = 20
-  engine            = "postgres"
-  instance_class    = var.db_size
-  password          = random_string.postgresql_password.result
-  # no special characters allowed
-  username = var.db_username
 
-  allow_major_version_upgrade = false
-  apply_immediately           = true
-  auto_minor_version_upgrade  = true
-  backup_retention_period     = var.db_backup_retention
-  backup_window               = var.db_backup_window
-  db_subnet_group_name        = aws_db_subnet_group.tfe.name
-  delete_automated_backups    = true
-  deletion_protection         = false
-  engine_version              = var.engine_version
-  identifier_prefix           = "${var.friendly_name_prefix}-tfe"
-  max_allocated_storage       = 0
-  multi_az                    = true
-  # no special characters allowed
-  db_name                = var.db_name
-  port                   = 5432
-  publicly_accessible    = false
-  skip_final_snapshot    = true
-  storage_encrypted      = true
-  kms_key_id             = var.kms_key_arn
-  storage_type           = "gp2"
-  vpc_security_group_ids = [aws_security_group.postgresql.id]
+
+module "rds_aurora" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+
+  name           = "test-aurora-db-postgres96"
+  engine         = "aurora-postgresql"
+  engine_version = "14.5"
+  instance_class = "db.r6g.large"
+  instances = {
+    one = {}
+    2 = {
+      instance_class = "db.r6g.2xlarge"
+    }
+  }
+  master_username = "tfe"
+  master_password          = random_string.postgresql_password.result
+  vpc_id               = var.network_id
+  db_subnet_group_name = aws_db_subnet_group.tfe.name
+  security_group_rules = {
+    ex1_ingress = {
+      cidr_blocks = var.network_private_subnet_cidrs
+    }
+    ex1_ingress = {
+      source_security_group_id = aws_security_group.postgresql.id
+    }
+  }
+
+  skip_final_snapshot = true
+  storage_encrypted   = true
+  apply_immediately   = true
+  monitoring_interval = 10
+
+  enabled_cloudwatch_logs_exports = ["postgresql"]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
+
+#  source  = "terraform-aws-modules/rds-aurora/aws"
+#  version = "~> 8.5.0"
+#
+#  name           = "test-aurora-db-postgres96"
+#  engine         = "aurora-postgresql"
+#  engine_version = "14"
+#  instance_class  = "db.r5.large"
+#
+#  master_username = "csteinmeyer"
+#  master_password = "password1234"
+#  create_db_parameter_group = false
+#  db_parameter_group_name         = "default"
+#  db_cluster_parameter_group_name = "default"
+#  db_parameter_group_family = "aurora-postgresql14"
+#
+#  vpc_id  = var.network_id
+#  db_subnet_group_name        = aws_db_subnet_group.tfe.name
+#
+#  autoscaling_enabled           = true
+#  
+#  security_group_rules = true ? {
+#    vpc_ingress = {
+#      cidr_blocks = var.network_private_subnet_cidrs
+#    }
+#  } : null
+#
+#  vpc_security_group_ids = [aws_security_group.postgresql.id]
+#
+#  storage_encrypted   = true
+#  apply_immediately   = true
+#  monitoring_interval = 10
+#
+#
+#  enabled_cloudwatch_logs_exports = ["postgresql"]
+#
+#  tags = {
+#    Environment = "dev"
+#    Terraform   = "true"
+#    ok_to_terminate   = "true"
+#    User   = "csteinmeyer"
+#  }
+#}
+
+#resource "aws_db_instance" "postgresql" {
+#  allocated_storage = 20
+#  engine            = "postgres"
+#  instance_class    = var.db_size
+#  password          = random_string.postgresql_password.result
+#  # no special characters allowed
+#  username = var.db_username
+#
+#  allow_major_version_upgrade = false
+#  apply_immediately           = true
+#  auto_minor_version_upgrade  = true
+#  backup_retention_period     = var.db_backup_retention
+#  backup_window               = var.db_backup_window
+#  db_subnet_group_name        = aws_db_subnet_group.tfe.name
+#  delete_automated_backups    = true
+#  deletion_protection         = false
+#  engine_version              = var.engine_version
+#  identifier_prefix           = "${var.friendly_name_prefix}-tfe"
+#  max_allocated_storage       = 0
+#  multi_az                    = true
+#  # no special characters allowed
+#  db_name                = var.db_name
+#  port                   = 5432
+#  publicly_accessible    = false
+#  skip_final_snapshot    = true
+#  storage_encrypted      = true
+#  kms_key_id             = var.kms_key_arn
+#  storage_type           = "gp2"
+#  vpc_security_group_ids = [aws_security_group.postgresql.id]
+#}
+#
--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -1,27 +1,159 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+#output "endpoint" {
+#  value       = rds.aurora.endpoint
+#  description = "The connection endpoint of the PostgreSQL RDS instance in address:port format."
+#}
+#
+#output "name" {
+#  value       = rds_aurora.postgresql.name
+#  description = "The name of the PostgreSQL RDS instance."
+#}
+#
+#output "password" {
+#  value       = rds_aurora.postgresql.password
+#  description = "The password of the main PostgreSQL user."
+#}
+#
+#output "username" {
+#  value       = rds_aurora.postgresql.username
+#  description = "The name of the main PostgreSQL user."
+#}
+#
+#output "parameters" {
+#  value       = var.db_parameters
+#  description = "PostgreSQL server parameters for the connection URI."
+#}
+
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+#------------------------------------------------------------------------------
+# Networking
+#------------------------------------------------------------------------------
+
+output "db_cluster_port" {
+  description = "Configured port that the database cluster is listening on."
+  value       = module.rds_aurora.cluster_port
+}
+
+output "db_cluster_hosted_zone_id" {
+  description = "The Route53 Hosted Zone ID of the endpoint"
+  value       = module.rds_aurora.cluster_hosted_zone_id
+}
+
+output "db_security_group_id" {
+  description = "The security group ID of the cluster"
+  value       = module.rds_aurora.security_group_id
+}
+
+output "db_subnet_group_name" {
+  description = "Database subnet group name"
+  value       = module.rds_aurora.db_subnet_group_name
+}
+
+#------------------------------------------------------------------------------
+# Cluster
+#------------------------------------------------------------------------------
+
+output "db_global_cluster_id" {
+  description = "ID of the global cluster that has been created (if specified)."
+  #value       = try(time_sleep.cluster[0].triggers["global_cluster_id"], null)
+  value       = module.rds_aurora.cluster_id
+}
+
+output "db_cluster_arn" {
+  description = "Amazon Resource Name (ARN) of cluster"
+  value       = module.rds_aurora.cluster_arn
+}
+
+output "db_cluster_id" {
+  description = "The RDS Cluster Identifier"
+  value       = module.rds_aurora.cluster_id
+}
+
+output "db_cluster_resource_id" {
+  description = "The RDS Cluster Resource ID"
+  value       = module.rds_aurora.cluster_resource_id
+}
+
+output "db_cluster_members" {
+  description = "List of RDS Instances that are a part of this cluster"
+  value       = module.rds_aurora.cluster_members
+}
+
+output "db_cluster_endpoint" {
+  description = "Writer endpoint for the cluster"
+  value       = module.rds_aurora.cluster_endpoint
+}
+
 output "endpoint" {
-  value       = aws_db_instance.postgresql.endpoint
-  description = "The connection endpoint of the PostgreSQL RDS instance in address:port format."
+  description = "Writer endpoint for the cluster"
+  value       = module.rds_aurora.cluster_endpoint
+}
+
+output "db_cluster_reader_endpoint" {
+  description = "A read-only endpoint for the cluster, automatically load-balanced across replicas"
+  value       = module.rds_aurora.cluster_reader_endpoint
+}
+
+output "db_cluster_engine_version_actual" {
+  description = "The running version of the cluster database"
+  value       = module.rds_aurora.cluster_engine_version_actual
 }
 
 output "name" {
-  value       = aws_db_instance.postgresql.name
-  description = "The name of the PostgreSQL RDS instance."
+  description = "Name for an automatically created database on cluster creation"
+  value       = module.rds_aurora.cluster_database_name
 }
 
-output "password" {
-  value       = aws_db_instance.postgresql.password
-  description = "The password of the main PostgreSQL user."
+output "db_cluster_instances" {
+  description = "A map of cluster instances and their attributes"
+  value       = module.rds_aurora.cluster_instances
+}
+
+output "db_additional_cluster_endpoints" {
+  description = "A map of additional cluster endpoints and their attributes"
+  value       = module.rds_aurora.additional_cluster_endpoints
+}
+
+output "db_cluster_role_associations" {
+  description = "A map of IAM roles associated with the cluster and their attributes"
+  value       = module.rds_aurora.cluster_role_associations
 }
 
 output "username" {
-  value       = aws_db_instance.postgresql.username
-  description = "The name of the main PostgreSQL user."
+  description = "The database user that was created"
+  value       = try(module.rds_aurora.cluster_master_username, "")
+  sensitive   = true
 }
 
-output "parameters" {
-  value       = var.db_parameters
-  description = "PostgreSQL server parameters for the connection URI."
+output "password" {
+  description = "The database user password"
+  value       = try(module.rds_aurora.cluster_master_password, "")
+  sensitive   = true
+}
+
+#------------------------------------------------------------------------------
+# Monitoring
+#------------------------------------------------------------------------------
+output "db_enhanced_monitoring_iam_role_name" {
+  description = "The name of the enhanced monitoring role"
+  value       = module.rds_aurora.enhanced_monitoring_iam_role_name
+}
+
+output "db_enhanced_monitoring_iam_role_arn" {
+  description = "The Amazon Resource Name (ARN) specifying the enhanced monitoring role"
+  value       = module.rds_aurora.enhanced_monitoring_iam_role_arn
+}
+
+output "db_enhanced_monitoring_iam_role_unique_id" {
+  description = "Stable and unique string identifying the enhanced monitoring role"
+  value       = module.rds_aurora.enhanced_monitoring_iam_role_unique_id
+}
+
+output "db_cluster_cloudwatch_log_groups" {
+  description = "Map of CloudWatch log groups created and their attributes"
+  value       = module.rds_aurora.db_cluster_cloudwatch_log_groups
 }
