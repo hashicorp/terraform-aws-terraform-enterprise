25643f46c6e7128f116a985632e4d5d5ca823c2e
Kosy Anyanwu <kosy.anyanwu@hashicorp.com>
2024-05-14 22:12:35 +0200

    update data

--- tests/standalone-mounted-amazon-linux-2023/data.tf
+++ tests/standalone-mounted-amazon-linux-2023/data.tf
@@ -17,12 +17,17 @@ data "aws_ami" "amazon_linux_2023" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*"]
+    values = ["al2023-ami-2023*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 
   owners = ["amazon"]
