559a43e0b97c5e81563ce98489bf00bcd54ca3f8
Kosy Anyanwu <kosy.anyanwu@hashicorp.com>
2024-05-15 13:23:51 +0200

    Support Amazon Linux 2023

--- variables.tf
+++ variables.tf
@@ -55,8 +55,8 @@ variable "distribution" {
   type        = string
   description = "(Required) What is the OS distribution of the instance on which Terraoform Enterprise will be deployed?"
   validation {
-    condition     = contains(["rhel", "ubuntu"], var.distribution)
-    error_message = "Supported values for distribution are 'rhel' or 'ubuntu'."
+    condition     = contains(["rhel", "ubuntu", "amazon-linux-2023"], var.distribution)
+    error_message = "Supported values for distribution are 'rhel', 'ubuntu' or amazon-linux-2023."
   }
 }
 
