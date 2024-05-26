b464c3a1a08218b3e9eea3f0b8cbe8a6e244c39f
Sandra Maria Peter <speter@hashicorp.com>
2024-05-26 00:50:44 -0400

    fix

--- examples/standalone-aurora/versions.tf
+++ examples/standalone-aurora/versions.tf
@@ -4,10 +4,6 @@
 terraform {
   required_version = ">= 0.14"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1"
