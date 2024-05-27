93223eb6d22cc4f93af798a4a9cf0047020e76e2
Sandra Maria Peter <speter@hashicorp.com>
2024-05-27 13:19:45 -0400

    update

---
+++ examples/standalone-aurora/versions.tf
@@ -0,0 +1,12 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+}
