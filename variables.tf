778cd3b0fe950ffefc614f9a7cd9033f1cabc2ba
Aaron Lane <aaron.lane@hashicorp.com>
2025-02-28 10:34:15 -0500

    Set aurora_postgres_engine_version default to 16

--- variables.tf
+++ variables.tf
@@ -258,7 +258,7 @@ variable "aurora_db_username" {
 
 variable "aurora_postgres_engine_version" {
   type        = string
-  default     = "16.2"
+  default     = "16"
   description = "Aurora PostgreSQL version."
 }
 
