8537951b48abb4f0dec98167f78e04ba30543d7b
John Kerry <john.kerry@hashicorp.com>
2025-03-05 17:53:21 -0500

    widening redis user permissions to fix replication

--- modules/redis-sentinel/files/script.sh
+++ modules/redis-sentinel/files/script.sh
@@ -4,15 +4,37 @@
 
 set -eu pipefail
 
+function retry {
+  local retries=$1
+  shift
+
+  local count=0
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** $count))
+    count=$(($count + 1))
+    if [ $count -lt $retries ]; then
+      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+      sleep $wait
+    else
+      echo "Retry $count/$retries exited $exit, no more retries left."
+      return $exit
+    fi
+  done
+  return 0
+
+}
+
+
 curl --noproxy '*' --fail --silent --show-error --location https://download.docker.com/linux/ubuntu/gpg \
 	| gpg --dearmor --output /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
 	"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
 	https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable" \
 	| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get --assume-yes update
-apt-get --assume-yes install docker-ce docker-ce-cli containerd.io redis-tools
-apt-get --assume-yes autoremove
+retry 10 apt-get --assume-yes update
+retry 10 apt-get --assume-yes install docker-ce docker-ce-cli containerd.io redis-tools
+retry 10 apt-get --assume-yes autoremove
 
 tfe_dir="/etc/redis"
 mkdir -p $tfe_dir
@@ -29,4 +51,5 @@ echo ${redis_conf} | base64 -d > $REDIS_CONF
 echo ${redis_init} | base64 -d > $REDIS_INIT
 chmod a+r $REDIS_CONF
 chmod a+x $SENTINEL_ENTRYPOINT
+chmod a+x $REDIS_INIT
 docker compose -f $tfe_dir/compose.yaml up -d
