fe48d1be686f12c6c331caa9c209d3caee40f204
John Kerry <john.kerry@hashicorp.com>
2025-03-06 11:37:25 -0500

    changing redis to username/password

--- modules/redis-sentinel/variables.tf
+++ modules/redis-sentinel/variables.tf
@@ -101,7 +101,7 @@ variable "redis_port" {
 variable "redis_sentinel_password" {
   description = "The password for the redis sentinel"
   type        = string
-  default     = "sentinelpassword"
+  default     = "sentinelpass"
 }
 
 variable "redis_sentinel_username" {
@@ -125,7 +125,7 @@ variable "redis_password" {
 variable "redis_username" {
   description = "The username for the admin user in redis server instances."
   type        = string
-  default     = ""
+  default     = "redisuser"
 }
 
 variable "redis_use_password_auth" {
