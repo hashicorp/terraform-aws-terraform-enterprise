44b2a0998c5f3027877315fd7c65efd311e9dad1
Kosy Anyanwu <kosy.anyanwu@hashicorp.com>
2025-06-04 11:20:09 +0200

    Update branch

--- main.tf
+++ main.tf
@@ -170,7 +170,7 @@ module "aurora_database" {
 # Docker Compose File Config for TFE on instance(s) using Flexible Deployment Options
 # ------------------------------------------------------------------------------------
 module "runtime_container_engine_config" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/runtime_container_engine_config?ref=kosy/add-admin-port"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/runtime_container_engine_config?ref=main"
   count  = var.is_replicated_deployment ? 0 : 1
 
   tfe_license = var.hc_license
