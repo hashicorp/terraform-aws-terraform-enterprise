7ac0dcd567bd82fa88b44081cab4d4322dc25267
sandrampeter <speter@hashicorp.com>
2025-06-04 14:22:22 -0400

    update

--- modules/redis-standalone-mtls/main.tf
+++ modules/redis-standalone-mtls/main.tf
@@ -67,10 +67,7 @@ resource "aws_autoscaling_group" "redis" {
   max_size            = 1
   desired_capacity    = 1
   vpc_zone_identifier = var.network_subnets_private
-  target_group_arns = concat(
-    [for tg in aws_lb_target_group.redis_tg : tg.arn],
-    [for tg in aws_lb_target_group.redis_tg : tg.arn]
-  )
+  target_group_arns = aws_lb_target_group.redis_tg.tg.arn
 
   # Increases grace period for any AMI that is not the default Ubuntu
   # since RHEL has longer startup time
