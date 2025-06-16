9747340bfddd8def86a1e0e895923a0395e35520
ajmera-naman <naman.ajmera@hashicorp.com>
2025-06-16 18:28:01 +0530

    use postgres-mtls branch

--- main.tf
+++ main.tf
@@ -201,7 +201,7 @@ module "aurora_database" {
 # Docker Compose File Config for TFE on instance(s) using Flexible Deployment Options
 # ------------------------------------------------------------------------------------
 module "runtime_container_engine_config" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/runtime_container_engine_config?ref=main"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/runtime_container_engine_config?ref=postgres-mtls"
   count  = var.is_replicated_deployment ? 0 : 1
 
   tfe_license = var.hc_license
