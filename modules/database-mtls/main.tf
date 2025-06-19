7f4cdbfecad3c404870f6d22e691e51f52dcf27b
ajmera-naman <naman.ajmera@hashicorp.com>
2025-06-19 13:46:15 +0530

    remove key-pair

--- modules/database-mtls/main.tf
+++ modules/database-mtls/main.tf
@@ -58,7 +58,7 @@ resource "aws_instance" "postgres" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.postgresql.id]
   iam_instance_profile        = aws_iam_instance_profile.nginx_instance_profile.name
-  key_name                    = aws_key_pair.ec2_key.key_name
+  # key_name                    = aws_key_pair.ec2_key.key_name
 
   user_data = templatefile("${path.module}/templates/startup.sh.tpl", {
     POSTGRES_USER     = var.db_username
