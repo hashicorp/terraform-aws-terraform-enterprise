4a785f8bab3073736e73172227ea985efac38533
ajmera-naman <naman.ajmera@hashicorp.com>
2025-06-20 20:48:54 +0530

    chore: update doormat version

--- modules/database-mtls/versions.tf
+++ modules/database-mtls/versions.tf
@@ -1,25 +1,21 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
-  required_version = ">= 0.14"
-
+  required_version = "~> 1.6.0"
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
     doormat = {
       source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "0.0.7"
+      version = "~> 0.0.15"
     }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
     }
     tls = {
       source  = "hashicorp/tls"
