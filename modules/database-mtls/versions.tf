4b8dbe141a135116dbf367854ef0c5bb8581f077
ajmera-naman <naman.ajmera@hashicorp.com>
2025-06-21 14:07:35 +0530

    comment doormat provider

--- modules/database-mtls/versions.tf
+++ modules/database-mtls/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = "~> 1.6.0"
+  # required_version = "~> 1.6.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    doormat = {
-      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "~> 0.0.15"
-    }
+    # doormat = {
+    #   source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+    #   version = "~> 0.0.15"
+    # }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2.0"
