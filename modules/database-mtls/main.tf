a6a4f2709cba86d06c358afd15ad76fbe209b1de
ajmera-naman <naman.ajmera@hashicorp.com>
2025-06-28 18:04:54 +0530

    add ssh ingress

--- modules/database-mtls/main.tf
+++ modules/database-mtls/main.tf
@@ -26,6 +26,16 @@ resource "aws_security_group_rule" "postgresql_ingress" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "postgresql_ingress_ssh" {
+  security_group_id = aws_security_group.postgresql.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "postgresql_tfe_egress" {
   security_group_id = aws_security_group.postgresql.id
   type              = "egress"
