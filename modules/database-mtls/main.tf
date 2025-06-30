9e8fab3f5c497d1cd30dedd7e71563749c873142
ajmera-naman <naman.ajmera@hashicorp.com>
2025-06-30 22:35:17 +0530

    chore: fix space

--- modules/database-mtls/main.tf
+++ modules/database-mtls/main.tf
@@ -113,7 +113,7 @@ resource "null_resource" "generate_certificates" {
     inline = [
       "sleep 60",
       "chmod +x /home/ubuntu/certificate_generate.sh",
-      "sudo postgres_user = ${var.db_username} postgres_db = ${var.db_name} postgres_client_cert=${var.postgres_client_certificate_secret_id} postgres_client_key=${var.postgres_client_key_secret_id} postgres_client_ca=${var.postgres_ca_certificate_secret_id} /home/ubuntu/certificate_generate.sh"
+      "sudo postgres_user=${var.db_username} postgres_db=${var.db_name} postgres_client_cert=${var.postgres_client_certificate_secret_id} postgres_client_key=${var.postgres_client_key_secret_id} postgres_client_ca=${var.postgres_ca_certificate_secret_id} /home/ubuntu/certificate_generate.sh"
     ]
   }
 }
