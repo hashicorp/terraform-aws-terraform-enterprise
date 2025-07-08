c7f21a5c205ddede5cae04045d20a5888b8b1e98
Vanessa MacDougal <vanessa.macdougal@hashicorp.com>
2025-07-08 17:15:29 -0400

    point to redis-sentinel branch of terraform-random-tfe-utility

--- main.tf
+++ main.tf
@@ -201,7 +201,7 @@ module "aurora_database" {
 # Docker Compose File Config for TFE on instance(s) using Flexible Deployment Options
 # ------------------------------------------------------------------------------------
 module "runtime_container_engine_config" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/runtime_container_engine_config?ref=main"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/runtime_container_engine_config?ref=TF-24345/redis-sentinel"
   count  = var.is_replicated_deployment ? 0 : 1
 
   tfe_license = var.hc_license
@@ -280,7 +280,7 @@ module "runtime_container_engine_config" {
 # AWS cloud init used to install and configure TFE on instance(s) using Flexible Deployment Options
 # --------------------------------------------------------------------------------------------------
 module "tfe_init_fdo" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/tfe_init?ref=main"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/tfe_init?ref=TF-24345/redis-sentinel"
   count  = var.is_replicated_deployment ? 0 : 1
 
   cloud             = "aws"
@@ -319,7 +319,7 @@ module "tfe_init_fdo" {
 # TFE and Replicated settings to pass to the tfe_init_replicated module for replicated deployment
 # --------------------------------------------------------------------------------------------
 module "settings" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/settings?ref=main"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/settings?ref=TF-24345/redis-sentinel"
   count  = var.is_replicated_deployment ? 1 : 0
 
   # TFE Base Configuration
@@ -381,7 +381,7 @@ module "settings" {
 # AWS user data / cloud init used to install and configure TFE on instance(s)
 # -----------------------------------------------------------------------------
 module "tfe_init_replicated" {
-  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/tfe_init_replicated?ref=main"
+  source = "git::https://github.com/hashicorp/terraform-random-tfe-utility//modules/tfe_init_replicated?ref=TF-24345/redis-sentinel"
   count  = var.is_replicated_deployment ? 1 : 0
 
   # TFE & Replicated Configuration data
