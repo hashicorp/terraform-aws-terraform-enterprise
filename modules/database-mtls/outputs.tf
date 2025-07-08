8150c8da4cd815e13f032b517bd0e3fd25660caf
ajmera-naman <naman.ajmera@hashicorp.com>
2025-07-08 12:21:57 +0530

    use jsonencode for password

--- modules/database-mtls/outputs.tf
+++ modules/database-mtls/outputs.tf
@@ -9,7 +9,7 @@ output "name" {
 }
 
 output "password" {
-  value       = random_string.postgres_password.result
+  value       = jsonencode(random_string.postgres_password.result)
   description = "The password of the main PostgreSQL user."
 }
 
