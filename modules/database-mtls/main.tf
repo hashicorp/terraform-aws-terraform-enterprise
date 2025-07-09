17111816a4a8376d0813d6cc81c195b03aca21ed
ajmera-naman <naman.ajmera@hashicorp.com>
2025-07-09 14:54:30 +0530

    chore: use startup script

--- modules/database-mtls/main.tf
+++ modules/database-mtls/main.tf
@@ -107,14 +107,14 @@ resource "aws_instance" "postgres_db_instance" {
     Name = "Terraform-Postgres-mTLS"
   }
 
-  # user_data = templatefile("${path.module}/templates/certificate_generate.sh", {
-  #   POSTGRES_USER        = var.db_username
-  #   postgres_db_password = "postgres_postgres"
-  #   POSTGRES_DB          = var.db_name
-  #   POSTGRES_CLIENT_CERT = var.postgres_client_certificate_secret_id
-  #   POSTGRES_CLIENT_KEY  = var.postgres_client_key_secret_id
-  #   POSTGRES_CLIENT_CA   = var.postgres_ca_certificate_secret_id
-  # })
+  user_data = templatefile("${path.module}/templates/certificate_generate.sh", {
+    POSTGRES_USER        = var.db_username
+    POSTGRES_PASSWORD    = aws_route53_record.postgres_db_dns.fqdn
+    POSTGRES_DB          = var.db_name
+    POSTGRES_CLIENT_CERT = var.postgres_client_certificate_secret_id
+    POSTGRES_CLIENT_KEY  = var.postgres_client_key_secret_id
+    POSTGRES_CLIENT_CA   = var.postgres_ca_certificate_secret_id
+  })
 }
 
 resource "local_file" "postgres_db_private_key" {
@@ -155,7 +155,7 @@ resource "null_resource" "postgres_db_cert_generation" {
     inline = [
       "sleep 60",
       "chmod +x /home/ubuntu/certificate_generate.sh",
-      "sudo postgres_db_password='${random_string.postgres_db_password.result}' POSTGRES_USER=${var.db_username} POSTGRES_DB=${var.db_name} POSTGRES_CLIENT_CERT=${var.postgres_client_certificate_secret_id} POSTGRES_CLIENT_KEY=${var.postgres_client_key_secret_id} POSTGRES_CLIENT_CA=${var.postgres_ca_certificate_secret_id} /home/ubuntu/certificate_generate.sh"
+      "sudo POSTGRES_PASSWORD='${random_string.postgres_db_password.result}' POSTGRES_USER=${var.db_username} POSTGRES_DB=${var.db_name} POSTGRES_CLIENT_CERT=${var.postgres_client_certificate_secret_id} POSTGRES_CLIENT_KEY=${var.postgres_client_key_secret_id} POSTGRES_CLIENT_CA=${var.postgres_ca_certificate_secret_id} /home/ubuntu/certificate_generate.sh"
     ]
   }
 }
