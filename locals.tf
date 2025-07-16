a316decad1c3ba21edd8313f7b4e2aaa9b2f5379
sandrampeter <speter@hashicorp.com>
2025-07-15 23:30:33 -0400

    Update Redis module configuration to use redis_mtls_enabled for count logic

--- locals.tf
+++ locals.tf
@@ -9,6 +9,7 @@ locals {
   enable_database_module       = local.enable_external && var.enable_aurora == false
   enable_object_storage_module = local.enable_external
   enable_redis_module          = var.operational_mode == "active-active"
+  redis_mtls_enabled           = var.enable_redis_mtls || var.enable_sentinel_mtls
   fdo_operational_mode         = var.operational_mode
   ami_id                       = local.default_ami_id ? data.aws_ami.ubuntu.id : var.ami_id
   default_ami_id               = var.ami_id == null
