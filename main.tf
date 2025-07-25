e56f479d5eba08c06dfccde93c106e0cc2c4ee16
sandrampeter <speter@hashicorp.com>
2025-07-25 10:50:24 -0400

    Refactor Redis mTLS configuration for consistency and clarity

--- main.tf
+++ main.tf
@@ -330,7 +330,7 @@ module "tfe_init_fdo" {
   key_secret_id            = var.vm_key_secret_id == null ? null : var.vm_key_secret_id
 
   enable_sentinel_mtls               = var.enable_sentinel_mtls
-  enable_redis_mtls                  = var.enable_redis_mtls 
+  enable_redis_mtls                  = var.enable_redis_mtls
   redis_ca_certificate_secret_id     = var.redis_ca_certificate_secret_id == null ? null : var.redis_ca_certificate_secret_id
   redis_client_certificate_secret_id = var.redis_client_certificate_secret_id == null ? null : var.redis_client_certificate_secret_id
   redis_client_key_secret_id         = var.redis_client_key_secret_id == null ? null : var.redis_client_key_secret_id
--- modules/redis-sentinel/locals.tf
+++ modules/redis-sentinel/locals.tf
@@ -8,11 +8,11 @@ locals {
   sentinel_password        = try(random_password.sentinel_password[0].result, null)
   redis_user_data_template = "${path.module}/files/script.sh"
   redis_leader_user_data = templatefile(local.redis_user_data_template, {
-    redis_client_cert = var.redis_client_certificate_secret_id
-    redis_client_key  = var.redis_client_key_secret_id
-    redis_client_ca   = var.redis_ca_certificate_secret_id
+    redis_client_cert    = var.redis_client_certificate_secret_id
+    redis_client_key     = var.redis_client_key_secret_id
+    redis_client_ca      = var.redis_ca_certificate_secret_id
     enable_sentinel_mtls = var.enable_sentinel_mtls
-    redis_init = base64encode(file(local.redis_init_path))
+    redis_init           = base64encode(file(local.redis_init_path))
     redis_conf = base64encode(templatefile(local.redis_conf_path, {
       redis_username = local.redis_username
       redis_password = local.redis_password
