9f5b7c27e95e1d9b1d638747120af7c6f4e28593
sandrampeter <speter@hashicorp.com>
2025-07-31 12:38:10 -0400

    Fix redis sentinel monitor command to increment redis_port by 1

--- modules/redis-sentinel/files/sentinel_start_tls.sh
+++ modules/redis-sentinel/files/sentinel_start_tls.sh
@@ -16,7 +16,7 @@ mkdir -p /etc/redis
 
 # Generate the sentinel.conf file
 cat <<EOF > /etc/redis/sentinel.conf
-sentinel monitor ${redis_sentinel_leader_name} $${HOST_IP} ${redis_port} 1
+sentinel monitor ${redis_sentinel_leader_name} $${HOST_IP} ${redis_port+1} 1
 
 sentinel resolve-hostnames yes
 sentinel down-after-milliseconds ${redis_sentinel_leader_name} 5000
