3b924cacd92ee30ff4e5a9491879f59186240064
RAVI PRAKASH <ravi.prakash@hashicorp.com>
2025-11-13 11:14:39 +0530

    Set IAM_AUTH placeholder for TFE_DATABASE_PASSWORD
    TFE configuration validation requires a non-empty password field.
    Set 'IAM_AUTH' placeholder for IAM authentication mode to pass
    config validation while pgmultiauth handles actual authentication.

--- main.tf
+++ main.tf
@@ -295,8 +295,8 @@ module "runtime_container_engine_config" {
   database_name             = local.database.name
   # Use IAM username when IAM authentication is enabled, otherwise use admin username
   database_user             = local.database_passwordless_aws_use_iam ? var.db_iam_username : local.database.username
-  # Disable password for IAM auth - token will be generated at runtime
-  database_password         = local.database_passwordless_aws_use_iam ? null : local.database.password
+  # For IAM auth, set a special placeholder that TFE recognizes
+  database_password         = local.database_passwordless_aws_use_iam ? "IAM_AUTH" : local.database.password
   database_host             = local.database.endpoint
   database_parameters       = local.database.parameters
   database_use_mtls         = var.db_use_mtls
